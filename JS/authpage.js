9a18e880e8eccd40e8d124a1bebe21fbffd6ba3d
Toby 👹 <bolajibaasit14@gmail.com>
2026-01-24 22:56:55 +0100

    Add background image rotation functionality

---
+++ JS/authpage.js
@@ -0,0 +1,13 @@
+const images = [
+    "../assets/background_img_5.jpg",
+    "../assets/background_img_6.jpg",
+    "../assets/background_img_7.jpg",
+    "../assets/background_img_8.jpg"
+];
+let currentIndex = 0;
+function changeBackground() {
+    document.body.style.backgroundImage = `url("${images[currentIndex]}")`;
+    currentIndex = (currentIndex + 1) % images.length;
+}
+changeBackground();
+setInterval(changeBackground, 5000);
